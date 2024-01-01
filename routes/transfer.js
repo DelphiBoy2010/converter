46c7737e890e021e79cbf73da7f67d84a28c2300
patrick <patrick@digilite.ca>
2024-01-01 16:53:34 +0330

    fix bug

--- routes/transfer.js
+++ routes/transfer.js
@@ -5,48 +5,53 @@ const mysql = require('mysql');
 async function insertData(newObjects) {
     let con;
     let connection;
+    const dbMode = process.env.DB_Mode;
     try {
-        // const pool = await mariadb.createPool({
-        //     host: process.env.DB_Host,
-        //     user: process.env.DB_User,
-        //     password: process.env.DB_Password,
-        //     port: process.env.DB_Port,
-        //     database: process.env.DB_Name,
-        //     connectionLimit: 5
-        // });
-        // con = await pool.getConnection();
-
-        connection = await mysql.createConnection({
-            host: process.env.DB_Host,
-            user: process.env.DB_User,
-            password: process.env.DB_Password,
-            database: process.env.DB_Name,
-        });
-        await connection.connect();
-
-        // const query = "INSERT INTO wpas_tbl_item (Title, Category, Province_ID, UserID, BusinessID, Code) " +
-        //     "VALUES ('hi', 1, 2, 3, '123',222)";
-        //const result = await con.query(query);
-
+        if(dbMode === 'mariadb'){
+            const pool = await mariadb.createPool({
+                host: process.env.DB_Host,
+                user: process.env.DB_User,
+                password: process.env.DB_Password,
+                port: process.env.DB_Port,
+                database: process.env.DB_Name,
+                connectionLimit: 5
+            });
+            con = await pool.getConnection();
+        }else{
+            connection = await mysql.createConnection({
+                host: process.env.DB_Host,
+                user: process.env.DB_User,
+                password: process.env.DB_Password,
+                database: process.env.DB_Name,
+            });
+            await connection.connect();
+        }
         for (const item of newObjects) {
             const query = "INSERT INTO wpas_tbl_item (Title, UserID, PreCode, Province_ID, Category, Phone, Address,\n" +
                 "Instagram, Whatsapp, Email, WebSite, Mobile, PostCode, Latitude, Longitude,\n" +
-                "ExtraCommentHide, Tag, GenderID, Telegram, Aparat, businessID, searchKeys) " +
-                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-            const result = await connection.query(query, [item?.Title,item?.UserID,item?.PreCode,
+                "ExtraCommentHide, Tag, GenderID, Telegram, Aparat, businessID, searchKeys, IsUpdated) " +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            const values =[item?.Title,item?.UserID,item?.PreCode,
                 item?.Province_ID,item?.Category,item?.Phone,item?.Address,
                 item?.Instagram,item?.Whatsapp,item?.Email,item?.WebSite,item?.Mobile,
                 item?.PostCode,item?.Latitude,item?.Longitude,item?.ExtraCommentHide,item?.Tag,
                 item?.GenderID,item?.Telegram,
-                item?.Aparat,item?.businessID,item?.searchKeys]);
-            console.log('Inserted data successfully:', result);
+                item?.Aparat,item?.businessID,item?.searchKeys, item?.IsUpdated];
+            if(dbMode === 'mariadb'){
+                const result = await con.query(query, values);
+            }else{
+                const result = await connection.query(query, values);
+            }
         }
     } catch (error) {
         console.error('Error inserting data:', error);
 
     } finally {
-        // await con.end();
-        await connection.end();
+        if(dbMode === 'mariadb'){
+            await con.end();
+        }else{
+            await connection.end();
+        }
     }
 }
 async function transferData(filePath) {
@@ -104,17 +109,19 @@ async function transferData(filePath) {
         const Latitude = jsonItem?.seoDetails?.schemas[0]?.latitude;
         const Longitude = jsonItem?.seoDetails?.schemas[0]?.longitude;
         const ExtraCommentHide = '';
-        const Tag = 'balad-resturant-tehran';
         const GenderID = 0;
         const Telegram = '';
         const Aparat = '';
         //const businessID =jsonItem?.itemData?.token;
         const businessID = jsonItem?.seoDetails?.schemas[0]?.identifier;
+        // const Tag = 'balad-resturant-tehran';
+        const Tag = businessID;
         const searchKeys = jsonItem?.categoryText;
+        const IsUpdated = 0;
         //const businessID =jsonItem?.token;
         newObjects.push({Title, UserID, PreCode, Province_ID, Category, Phone, Address,
             Instagram, Whatsapp, Email, WebSite, Mobile, PostCode, Latitude, Longitude,
-            ExtraCommentHide, Tag, GenderID, Telegram, Aparat, businessID, searchKeys});
+            ExtraCommentHide, Tag, GenderID, Telegram, Aparat, businessID, searchKeys, IsUpdated});
     }
     await insertData(newObjects);
 }
